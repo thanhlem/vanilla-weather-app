3150ae50161e1ec3475b1914746060b9c65f915f
thanhlem <thanhlm.rsc@gmail.com>
2022-11-09 10:35:23 +0700

    add weather icon

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   celsius = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = celsius;
   cityElement.innerHTML = response.data.name;
@@ -36,6 +37,11 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function search(city) {
   let apiKey = "7474903e58d91475b44694a60ba263f4";
