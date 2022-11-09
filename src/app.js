da0833c8e5944bb89864fd3871d3c5d4ddf5b637
thanhlem <thanhlm.rsc@gmail.com>
2022-11-09 10:32:19 +0700

    search city and update temperature, humidity, wind

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-alert("Hello");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  celsius = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = celsius;
+  cityElement.innerHTML = response.data.name;
+  conditionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+function search(city) {
+  let apiKey = "7474903e58d91475b44694a60ba263f4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+search("New York");
